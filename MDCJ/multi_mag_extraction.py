90db3608493c7df0a9913d2f0fbe0b3049586620
Maarten <maarten.jansen@ziggo.nl>
2023-11-10 15:14:10 +0100

    Update multi_mag_extraction
    Improved readability of of stdout
    Fixed file listing issues

--- MDCJ/multi_mag_extraction.py
+++ MDCJ/multi_mag_extraction.py
@@ -1,10 +1,11 @@
+from datetime import datetime
+import multiprocessing
 import os
 import shutil
-import multiprocessing
 
 # Credentials
 __author__ = "M.D.C. Jansen"
-__version__ = "1.4"
+__version__ = "1.5"
 __date__ = "10/11/2023"
 
 # Folder paths
@@ -37,11 +38,10 @@ def extract(file_info, source_folder, dest_folder):
                 if (extract_cx <= cx < extract_cx_end) and \
                         (extract_cx < cx + cw <= extract_cx_end):
                     if (extract_cy <= cy < extract_cy_end) and \
-                            (extract_cy < cy + ch < + extract_cy_end):
+                            (extract_cy < cy + ch <= extract_cy_end):
                         source_path = os.path.join(root, filename)
                         dest_path = os.path.join(dest_folder, filename)
                         shutil.move(source_path, dest_path)
-                        print("EXTRACTED:\t", filename, flush=True)
 
 
 def process_filtered(filename):
@@ -73,22 +73,39 @@ def process_sorted(filename):
 
 
 def main():
+    print("\33[96m[{time}][INFO]:\33[94m\tStart Extraction".format(time=datetime.now().strftime("%H:%M:%S")))
+
+    filtered_files = []
+    sorted_files = []
+
     for root, dirs, filenames in os.walk(filtered_folder):
-        filtered_files = filenames
+        filtered_files.extend(filenames)
+    print("[{time}][INFO]:\tListed all files from filterd_folder"
+          .format(time=datetime.now().strftime("%H:%M:%S")))
+
     for root, dirs, filenames in os.walk(sorted_folder):
-        sorted_files = filenames
+        sorted_files.extend(filenames)
+    print("[{time}][INFO]:\tListed all files from sorted_folder"
+          .format(time=datetime.now().strftime("%H:%M:%S")))
 
     pool = multiprocessing.Pool(processes=num_workers)
 
+    print("[{time}][INFO]:\tStart extraction using filtered files"
+          .format(time=datetime.now().strftime("%H:%M:%S")))
     pool.map(process_filtered, filtered_files)
-    print("[INFO]: Completed extraction from filtered files")
+    print("\33[92m[{time}][INFO]:\33[1m\tCompleted extraction from filtered files"
+          .format(time=datetime.now().strftime("%H:%M:%S")))
 
+    print("[{time}][INFO]:\tStart extraction using sorted files"
+          .format(time=datetime.now().strftime("%H:%M:%S")))
     pool.map(process_sorted, sorted_files)
-    print("[INFO]: Completed extraction from sorted files")
+    print("\33[92m[{time}][INFO]:\33[1m\tCompleted extraction from sorted files"
+          .format(time=datetime.now().strftime("%H:%M:%S")))
 
     pool.close()
     pool.join()
-    print("[INFO]: Extraction completed")
+    print("\33[96m[{time}][INFO]:\33[94m\tExtraction completed"
+          .format(time=datetime.now().strftime("%H:%M:%S")))
 
 
 if __name__ == "__main__":
